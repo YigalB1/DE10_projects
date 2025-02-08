b3f9e8022aa182f3d142b16c9e3322814ee92531
YigalB1 <yigalb@hotmail.com>
2025-02-08 07:24:01 +0200

    try2

---
+++ DE10_Lite_SDRAM/RTL/sdram_top.v
@@ -0,0 +1,30 @@
+module sdram_top(
+    input MAX10_CLK1_50, 
+    input [1:0] KEY,
+    output reg [7:0] LEDR
+);
+
+
+
+reg [9:0] leds;
+
+always @(posedge MAX10_CLK1_50) begin
+    //key_reg <=key;
+    if(KEY[0]==0) begin
+        leds[3:0]=4'b1111;
+		  leds[7:4]=4'b0000;
+    end
+	 else if(KEY[1]==0) begin
+		  leds[3:0]=4'b0000;
+		  leds[7:4]=4'b1111;
+	 end 
+	 else begin
+		  leds[3:0]=4'b1010;
+		  leds[7:4]=4'b0101;
+	 end 
+end
+
+always @ (posedge MAX10_CLK1_50) begin
+	LEDR[7:0] <= leds[7:0];
+end
+endmodule
