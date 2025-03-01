de22f409c0bb9d130169a5e497c3e6825af75346
YigalB1 <yigalb@hotmail.com>
2025-03-01 19:51:16 +0200

    Working

--- UART/Python_uart/utils.py
+++ UART/Python_uart/utils.py
@@ -2,6 +2,7 @@ import serial #pip onstall pyserial
 import sys
 import time
 import serial.tools.list_ports
+import random
 
 ftdi_name= "CP210x"
 baud=115200
@@ -54,3 +55,42 @@ def read_uart(ser):
             #print(".", end='') # no new line
             time.sleep(0.01)
     return None
+
+def test_many(_ser,_num):
+    good_num=0
+    bad_num=0
+    
+    t_start = time.time() 
+    for i in range(_num):
+        random_byte = random.randint(0, 255)
+        #print(random_byte)
+        my_byte = bytes([random_byte])
+        send_byte(_ser,my_byte)
+        #print("Byte sent  ", end='')
+
+        ret_byte=read_uart(_ser)
+        if ret_byte is None:
+            print("failed to read port while in Loop. exiting.")
+            return "failed",good_num,bad_num
+        #print("Got: "+ str(ret_byte))
+
+        sent_val = my_byte[0]
+        ret_val = ret_byte[0] 
+        #print(sent_val)
+        #print(ret_val)
+
+
+        #if(sent_val+1==ret_val):
+        if(my_byte[0]+1==ret_byte[0]):
+            good_num+=1
+        else:
+            bad_num+=1
+
+    t_end = time.time() 
+    print(str(t_end-t_start))
+    elapsed_time = t_end-t_start
+    print(f"Elapsed time: {elapsed_time:.4f} seconds")
+
+    print("Good: "+str(good_num) + "     Bad: "+str(bad_num)) 
+    return "passed",good_num,bad_num
+    #_ser.close()
